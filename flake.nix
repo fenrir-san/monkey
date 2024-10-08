88bb6b16c47ab7e0046da55faf63b34fad61f406
Vineet Agarwal <vineetagarwal2402@gmail.com>
2024-10-07 23:14:46 -0700

    Add dev shell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "MonkeyLang";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go
+      ];
+      shellHook = ''zsh; exit'';
+    };
+  };
+}
